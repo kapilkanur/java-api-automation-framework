54d97c766f0d84315ac6b50f2f23519fea2cd4a0
Kapil Kanur <kanurkapil@gmail.com>
2023-06-26 12:59:34 +0530

    Added logging support

--- src/main/java/com/kk/restful_booker/configuration/TestConfigurationParser.java
+++ src/main/java/com/kk/restful_booker/configuration/TestConfigurationParser.java
@@ -1,15 +1,20 @@
 package com.kk.restful_booker.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
 public class TestConfigurationParser {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestConfigurationParser.class);
+
     public TestConfiguration readConfiguration(String filePath) throws Exception {
+
         ObjectMapper objectMapper = new ObjectMapper();
         TestConfiguration testConfiguration = objectMapper.readValue(new File(filePath), TestConfiguration.class);
-        System.out.println(testConfiguration.getServerUrl());
+        logger.debug(testConfiguration.getServerUrl());
         return testConfiguration;
     }
 
--- src/test/java/com/kk/restful_booker/sample_cases/SampleTest.java
+++ src/test/java/com/kk/restful_booker/sample_cases/SampleTest.java
@@ -1,12 +1,17 @@
 package com.kk.restful_booker.sample_cases;
 
 import base_classes.RestfulBookerBaseClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 public class SampleTest extends RestfulBookerBaseClass {
 
+    private static final Logger logger = LoggerFactory.getLogger(SampleTest.class);
+
     @Test
     public void firstTest() {
+        logger.info("First Test");
 
     }
 
